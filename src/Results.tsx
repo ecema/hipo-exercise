3188ca4f160102830bed02a0ced0e1f957f0f38c
Ecem Afacan <ecem.afacan@checkbox.com.tr>
2021-08-03 00:37:33 +0300

    detail modal

--- src/Results.tsx
+++ src/Results.tsx
@@ -2,12 +2,56 @@ import React, { useState } from 'react';
 import amphibian from './amphibian-chameleon-2.png'
 import './App.css';
 import { useLocation } from 'react-router-dom';
+import { createApi } from "unsplash-js";
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 
 function Results() {
-  const [selectedPic, setSelectedPic] = useState({ alt_description: "", urls: { full: "" }, user: { name: "" ,location:""} });
+
+
+  const [selectedPic, setSelectedPic] = useState({
+    alt_description: "", urls: { full: "" },
+    user: { name: "", location: "" }, links: { download_location: "" }
+  });
   const [showDetail, setShowDetail] = useState(false);
   const location = useLocation();
+
+  const containerStyle = {
+    width: '400px',
+    height: '400px'
+  };
+
+  const center = {
+    lat: 3.745,
+    lng: -38.523
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyANkiGfse4vn08C18ZZkqizbz8OxMKd-7c"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    // const bounds = new window.google.maps.LatLngBounds();
+    // map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+
+  const api = createApi({
+    accessKey: "ZNSY0Eh9GhFKeOrkxRQeYWk8105EqJt2c4-ny2Zu6Lo"
+  });
+  function download() {
+    api.photos.trackDownload({ downloadLocation: selectedPic.links.download_location })
+      .then(res => console.log(res)).catch(err => console.log(err))
+  }
+
 
   var pics = location.state as Array<Object>;
   return (
@@ -26,6 +70,22 @@ function Results() {
             <span>{selectedPic.user.name}</span>
             <span>{selectedPic.user.location}</span>
             <button onClick={() => setShowDetail(false)}>close me</button>
+            <button onClick={() => download()}>download</button>
+
+            isLoaded ? (
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={10}
+              onLoad={onLoad}
+              onUnmount={onUnmount}
+              onClick={() => <a href="http://maps.google.com/maps?saddr=New+York&daddr=San+Francisco">Route New York -- San Francisco</a>
+              }
+            >
+              { /* Child components, such as markers, info windows, etc. */}
+              <></>
+            </GoogleMap>
+            ) : <></>
           </div>
         </div>)
         : null}
